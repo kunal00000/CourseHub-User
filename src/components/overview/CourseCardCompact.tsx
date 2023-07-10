5092ee2188b64e8a893cafb15e92685954782f68
kunal00000 <kunalverma2468@gmail.com>
2023-07-11 01:49:26 +0530

    component: compact course card component done

---
+++ src/components/overview/CourseCardCompact.tsx
@@ -0,0 +1,35 @@
+import { Badge, Group, Notification } from "@mantine/core";
+import { Course } from "../../types/course";
+
+const CourseCardCompact = ({ course }: { course: Course }) => {
+  const colors = [
+    "blue",
+    "cyan",
+    "teal",
+    "green",
+    "yellow",
+    "red",
+    "gray",
+    "pink",
+    "orange",
+    "indigo",
+    "violet",
+    "grape",
+    "lime",
+    "dark",
+  ];
+  return (
+    <Notification
+      title={course.title}
+      radius={"md"}
+      color={colors[Math.floor(Math.random() * colors.length)]}
+      withCloseButton={false}
+    >
+      <Group>
+        <Badge color="yellow">{course.price}$</Badge>
+      </Group>
+    </Notification>
+  );
+};
+
+export default CourseCardCompact;
